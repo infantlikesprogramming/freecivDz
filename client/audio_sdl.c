2188c510fa2246f064841a3a29866b05ed823b70
infantlikesprogramming <hadu@dickinson.edu>
2020-11-05 19:13:41 -0500

    add sound control feature

--- client/audio_sdl.c
+++ client/audio_sdl.c
@@ -57,7 +57,7 @@ static double sdl_audio_volume;
 **************************************************************************/
 static void sdl_audio_set_volume(double volume)
 {
-  Mix_VolumeMusic(volume * MIX_MAX_VOLUME);
+  Mix_VolumeMusic(sdl_audio_volume * MIX_MAX_VOLUME);
   Mix_Volume(-1, volume * MIX_MAX_VOLUME);
   sdl_audio_volume = volume;
 }
